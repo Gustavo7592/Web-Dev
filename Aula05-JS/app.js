945a5a8c432c487dd0ab91b5db33e57ccc7e64f0
Gustavo Cavalcanti <gucavalcanti2018@gmail.com>
2025-04-03 22:42:31 -0300

    Aula 5 javascript

---
+++ Aula05-JS/app.js
@@ -0,0 +1,19 @@
+// alert("Hello World")
+//let nome = prompt("Qual é o seu nome")
+//alert(nome)
+//let resultado = confirm("Você deseja seguir?")
+//console.log(`Olá ${nome},tudo bem?`)
+
+// Exercicio - 1
+
+// let nome = prompt("Qual seu nome?")
+// let idade = prompt("Qual sua idade")
+// let profissao = prompt("Qual sua profissao")
+// alert(`Olá ${nome}, sua profissao é ${profissao} e sua idade é ${idade}`)
+// let resultado = confirm("Certo?")
+
+let numero1 = parseInt(prompt("Digite um numero"))
+let numero2 = parseInt(prompt("Digite outro numero"))
+
+let resultado = numero1 + numero2
+alert(String(resultado))
