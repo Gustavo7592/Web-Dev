541eae2c718f3d0cf98337f70604a3806655e35a
Gustavo Cavalcanti <gucavalcanti2018@gmail.com>
2025-04-24 22:42:49 -0300

    Aula 07 Finalizada

---
+++ Aula07/app.js
@@ -0,0 +1,91 @@
+// function exibirNome(){
+//     console.log("Angel Romero")
+// }
+
+// exibirNome()
+
+// function pegaNome(){
+//     let nome = "Gustavo"
+//     return nome
+// }
+
+// function exibirNome(nomeUsuario, sobrenome){
+//     console.log(nomeUsuario)
+//     let resultado = pegaNome()
+//     let nome = ""
+//     console.log(resultado)
+// }
+
+// exibirNome("Gustavo", "Cavalcanti")
+
+// function somaDoisNumeros(primeiroNumero, segundoNumero = 0){
+//     return primeiroNumero + segundoNumero
+// }
+
+// console.log(somaDoisNumeros(5,17))
+// console.log(somaDoisNumeros(28))
+
+// const somaNumeros = numero1 => console.log(numero1)
+// const somaNumeros = (numero1, numero2) => numero1 + numero2
+
+// DESAFIO
+
+// //Função para pegar nome
+
+// function pegaNome(){
+//     let nome = prompt("Digite seu nome: ")
+//     return nome
+// }
+
+// // Função para pegar idade
+
+// function pegaIdade(){
+//     let idade = prompt("Digite sua idade: ")
+//     return idade
+// }
+
+// //Função mensagem
+
+// function mensagem(nome, idade){
+//     alert(`Seu nome é ${nome} e sua idade é ${idade}`)
+// }
+
+// //função para executar as outras funçoes
+
+// function iniciarProg(){
+//     let progIdade = pegaIdade()
+//     let progNome = pegaNome()
+//     mensagem(progIdade, progNome)
+// }
+
+// //executa o programa
+// iniciarProg()
+
+// Propiedades e metodos
+
+// let nome = "Gustavo"
+
+// console.log(nome)
+// console.log(nome.length)
+// console.log(nome.toUpperCase())
+// console.log(nome.toLowerCase())
+// console.log(nome.includes("@"))
+// console.log(nome.trim())
+
+// let numero = 5.32132321
+
+// console.log(numero.toFixed(2))
+// console.log(isNaN(numero))
+
+
+// // Arredonda para baixo
+// console.log(Math.floor(Math.random()*11))
+// // Arredonda para cima
+// console.log(Math.ceil(Math.random()*11))
+// // Arredonda justo
+// console.log(Math.round(Math.random()*11))
+
+// let dataAtual = new Date()
+// console.log(dataAtual.getHours())
+// console.log(dataAtual)
+
